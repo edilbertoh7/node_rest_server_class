a1a827adbaf6ddb025b181114dd952d970fde46b
edilbertoh7 <edilberto7@gmail.com>
2024-02-21 12:11:52 -0500

    se valida si el campo rol existe en la DB

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete } = require('../controllers/usuarios');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 const router = Router();
 
 router.get('/', usuariosGet);
@@ -12,7 +13,14 @@ router.post('/',[
   check('password', 'La contraseña debe contener al menos 6 caracteres').isLength({min: 6}),
   //verifico que el correo sea valido
   check('correo', 'El correo no tiene un formato valido').isEmail(),
-  check('rol', 'El rol no es valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  // check('rol', 'El rol no es valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+  //verifico que el rol exista en DB
+  check('rol').custom( async(rol ='')=>{
+    const existeRol = await Role.findOne({rol});
+    if (!existeRol) {
+      throw new Error(`El rol ${rol} no se encuantra registrado en la base de datos`);
+    }
+  }),
   //invoco el middleware personalizado para verificar todos los errores
   validarCampos
 ], usuariosPost);
