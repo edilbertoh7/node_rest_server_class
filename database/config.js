e4fe7fd79fecf004e287b63edc78eb9671fa18c5
edilbertoh7 <edilberto7@gmail.com>
2024-03-03 18:58:06 -0500

    cambio en archivo config.js

--- database/config.js
+++ database/config.js
@@ -3,13 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect(process.env.MONGODB_CNN, {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-            // useCreateIndex: true,
-
-            // useFindAndModify: false
-        });
+        await mongoose.connect(process.env.MONGODB_CNN);
 
         console.log('Base de datos mongo online');
 
