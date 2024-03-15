35900ea1958e64a07b251711f84b0aafb533e4af
edilbertoh7 <edilberto7@gmail.com>
2024-03-14 22:03:16 -0500

    creacion de archivo .env_example

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect(process.env.MONGODB_CNN);
+        await mongoose.connect(process.env.MONGODB_CNN_LOCAL);
 
         console.log('Base de datos mongo online');
 
